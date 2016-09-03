7f82dfa7af8734ecf742fd830267688c633731f0
Manoel Jose De Freitas Neto <manoel.josefneto@gmail.com>
2016-09-02 23:25:14 -0300

    implementando redux com biblioteca redux

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import expect from 'expect';
-
+import {createStore} from 'Redux';
 
 const counter = (state=0, action) =>{
     switch (action.type) {
@@ -13,28 +12,5 @@ const counter = (state=0, action) =>{
     }
 }
 
-expect(
-    counter(0, { type: 'INCREMENT' })
-).toEqual(1);
+const store = createStore(counter);
 
-expect(
-    counter(1, { type: 'INCREMENT' })
-).toEqual(2);
-
-expect(
-    counter(3, { type: 'INCREMENT' })
-).toEqual(4);
-
-expect(
-    counter(3, { type: 'DECREMMENT' })
-).toEqual(2);
-
-expect(
-    counter(2, { type: 'DECREMMENT' })
-).toEqual(1);
-
-expect(
-    counter(2, { type: 'DECREMMENT' })
-).toEqual(1);
-
-console.log('test passed!')
